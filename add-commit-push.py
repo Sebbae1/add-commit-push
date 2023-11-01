8b946742ad3a85c444e3050ea210e2b4cfe9a43d
[[Sebbae1]] <[[seb.basti2004@gmail.com]]>
2023-11-01 10:31:31 -0500

    Update files.

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,20 +1,16 @@
 import os
 
 print("Add, Commit, Push with Python")
-#print("\ngit status")
-#os.system("git status")
-#os.system("git add -A")
+print("\ngit status")
+os.system("git status")
+print("\ngit add -A")
+os.system("git add -A")
+print("\ngit git commit -m 'Update Files'")
 os.system("git commit -m 'Update files.'")
-#s.system("git push")
-
-
-
-
+print("git push")
+os.system("git push")
 
 username = input("Enter Username:")
-
-
-
 
 print("Results:")
 print("Queued")
